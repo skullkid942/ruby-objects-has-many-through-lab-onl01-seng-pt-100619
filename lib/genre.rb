4bad75eece14c1a7482bf100d7223268c043b25e
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-14 14:02:11 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,26 @@
+class Genre 
+  
+  @@all = []
+  attr_reader :name, :genre
+  attr_accessor :song 
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+
+  def self.all
+    @@all
+  end 
+  
+  def songs 
+    Song.all.select{|song| song.genre == self}  
+  end
+  
+  def artists
+    songs.map do |song| 
+      song.artist
+    end 
+  end
+
+end
