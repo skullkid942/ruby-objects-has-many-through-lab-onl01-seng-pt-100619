f4006646faa74b396ddecc1208bff6b3f492e27b
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-14 14:37:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   @@all = []
   attr_accessor :name, :genre, :artist 
   
-  def initialize(name,genre,artist)
+  def initialize(name,artist,genre)
     @name = name
     @artist = artist
     @genre = genre
