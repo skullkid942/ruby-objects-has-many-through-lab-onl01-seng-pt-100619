85ee8b9fdfffb90d7ae26a4ae2a6f271210bcdc3
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-14 14:29:46 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,7 +16,7 @@ class Doctor
     Appointment.all.select {|appointment| appointment.doctor == self}
   end 
   
-  def new_appointment(date,patient)
+  def new_appointment(patient,date)
     appointment = Appointment.new(date,patient,self)
   end
   
